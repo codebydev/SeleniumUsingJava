b069bdeab4212ab4a52caf0bc7e6de19bad9536b
CodeByDev <codebydev@gmail.com>
2018-02-21 15:41:48 +0530

    Updated code as on 21st Feb

--- Basics.java
+++ Basics.java
@@ -2,7 +2,9 @@ import java.util.Set;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 
 public class Basics {
@@ -95,16 +97,132 @@ public class Basics {
 		System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
 		FirefoxDriver driver=new FirefoxDriver();
 		driver.get("https://netbanking.hdfcbank.com/netbanking/");
+		
+		// Step 1: before click windows size
+		System.out.println(driver.getTitle());
 		Set<String> windows=driver.getWindowHandles();
 		System.out.println("Before click : "+windows.size());
+		
 		driver.switchTo().frame(driver.findElement(By.tagName("frame")));
 		driver.findElement(By.linkText("Click here")).click();
 		Thread.sleep(6000);
+		
+		// Step 2: after click windows size
 		windows=driver.getWindowHandles();
 		System.out.println("After click : "+windows.size());
-		driver.switchTo().window();
-		//driver.findElement(By.name("fldLoginUserId")).sendKeys("1234");
+		for(String handle: windows)
+		{
+			System.out.println(handle);
+			driver.switchTo().window(handle);
+		}	
+		
+		// Step 3: Find the element in the new page and perform the operation
+		driver.findElement(By.name("fldLoginUserId")).sendKeys("1234");
+		System.out.println(driver.getTitle());
 		Thread.sleep(6000);
+		driver.close();
+		System.out.println("After close : "+driver.getWindowHandles().size());
+		driver.quit();
+	}
+	
+	@Test
+	public void doubleClick() throws InterruptedException
+	{
+		System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("file:///C:/Users/hanumanthargs/Downloads/HTML/DoubleClick.html");
+		
+		Actions actions=new Actions(driver);
+		Thread.sleep(6000);
+		actions
+		.doubleClick(driver.findElement(By.name("dblClick")))
+		.build()
+		.perform();
+		
+		Thread.sleep(6000);
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void contextClick() throws InterruptedException
+	{
+		System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("file:///C:/Users/hanumanthargs/Downloads/HTML/DoubleClick.html");
+		
+		Actions actions=new Actions(driver);
+		Thread.sleep(6000);
+		actions
+		.contextClick(driver.findElement(By.name("dblClick")))
+		.build()
+		.perform();
+		
+		Thread.sleep(6000);
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void dragAndDrop() throws InterruptedException
+	{
+		System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("file:///C:/Users/hanumanthargs/Downloads/HTML/DragAndDrop.html");
+		
+		Actions actions=new Actions(driver);
+		Thread.sleep(6000);
+		actions
+		.dragAndDrop(driver.findElement(By.id("draggable")), driver.findElement(By.id("droppable")))
+		.build()
+		.perform();
+		
+		Thread.sleep(6000);
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void keyDownAndKeyUp() throws InterruptedException
+	{
+		System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("file:///C:/Users/hanumanthargs/Downloads/HTML/Selectable.html");
+		
+		Actions actions=new Actions(driver);
+		Thread.sleep(6000);
+		actions
+		.keyDown(Keys.CONTROL)
+		.click(driver.findElement(By.name("one")))
+		.click(driver.findElement(By.name("six")))
+		.click(driver.findElement(By.name("eleven")))
+		.keyUp(Keys.CONTROL)
+		.build()
+		.perform();
+		
+		Thread.sleep(6000);
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void clickAndHoldAndRelease() throws InterruptedException
+	{
+		System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("file:///C:/Users/hanumanthargs/Downloads/HTML/Selectable.html");
+		
+		Actions actions=new Actions(driver);
+		Thread.sleep(6000);
+		actions		
+		.clickAndHold(driver.findElement(By.name("one")))
+		.moveToElement(driver.findElement(By.name("eleven")))
+		.release()		
+		.build()
+		.perform();
+		
+		Thread.sleep(6000);
+		
 		driver.quit();
 	}
 }
